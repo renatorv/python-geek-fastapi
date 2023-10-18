fe4a070a44393ea67bafa866f3209cb491585d86
renatorv <renatorv@gmail.com>
2023-10-18 08:45:16 -0300

    12 Prática O método GET_720p

--- secao03/main.py
+++ secao03/main.py
@@ -15,11 +15,18 @@ cursos = {
     },
 }
 
-@app.get('/curso')
+@app.get('/cursos')
 async def get_cursos():
     return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    curso = cursos[curso_id]
+    curso.update({"id": curso_id})
+    
+    return curso
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True, reload=True)
