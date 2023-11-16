77fef18547f42b67a6715923708bb64a542694a4
renatorv <renatorv@gmail.com>
2023-11-16 14:19:45 -0300

    24 Recapitulando_720p

--- secao03_p1/main.py
+++ secao03_p1/main.py
@@ -53,8 +53,8 @@ async def get_curso(curso_id: int = Path(default=None, title='ID do curso', desc
 @app.post('/cursos', status_code=status.HTTP_201_CREATED, response_model=Curso)
 async def post_cursos(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
-    cursos[next_id] = curso
-    del curso.id
+    curso.id = next_id
+    cursos.append(curso)
     return curso
 
 
--- secao03_p1/models.py
+++ secao03_p1/models.py
@@ -1,12 +1,26 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Curso(BaseModel):
     id: Optional[int] = None
     titulo: str
-    aulas: int
-    horas: int
+    aulas: int # mais de 12 aulas
+    horas: int # mais de 10 horas
+    
+    @validator('titulo')
+    def validar_titulo(cls, value: str):
+        palavras = value.split(' ')
+        
+        # Validação 1
+        if len(palavras) < 3:
+            raise ValueError('O título deve ter pelo menos 3 palavras.')
+        # Validação 2
+        if value.islower():
+            raise ValueError('O título deve ser capitalizado.')
+        
+        # Sempre deve ser retornado 'value', após a validação
+        return value
 
 cursos = [
     Curso(id=1, titulo='Programação para leigos', aulas=112, horas=58),
