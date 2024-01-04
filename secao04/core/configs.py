e6fc9eda5aa3d102236fc9b4eee378ed0ed9b958
renatorv <renatorv@gmail.com>
2024-01-04 07:45:03 -0300

    30 Prática Criando as tabelas_720p

--- secao04/core/configs.py
+++ secao04/core/configs.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
         Configurações gerais utilizadas na aplicação
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "postgresql+asyncpg://geek:university@localhost:5432/faculdade"
+    DB_URL: str = "postgresql+asyncpg://postgres:159753@localhost:5432/faculdade"
     DBBaseModel = declarative_base()
     
     class Config:
