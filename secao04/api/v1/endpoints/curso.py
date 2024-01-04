603cc75390e6260b4379194f12b8596e423908e8
renatorv <renatorv@gmail.com>
2024-01-04 14:24:13 -0300

    31 Prática Trabalhando no módulo api - Parte 1_720p

---
+++ secao04/api/v1/endpoints/curso.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from fastapi import APIRouter
+from fastapi import status
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Response
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.curso_model import CursoModel
+from schemas.curso_schema import CursoSchema
+from core.deps import get_session
+
+router = APIRouter()
+
+# POST curso
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
+async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(titulo=curso.titulo, aulas=curso.aulas, horas=curso.horas)
+    
+    db.add(novo_curso)
+    await db.commit()
+    
+    return novo_curso
+
+#GET cursos
+@router.get('/', response_model=List[CursoSchema])
+async def get_cursos(db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel)
+        result = await session.execute(query)
+        cursos: List[CursoModel] = result.scalars().all()
+        
+        return cursos
+        
